7a1a9b1f85ea22af8a09d9214c3d040b82d8ef71
AdrianSucahyo <adrianpsucahyo@gmail.com>
2025-03-28 12:16:00 -0600

    Finished Baseline H533

---
+++ Audio_Synthesizer_H533/Core/Src/main.c
@@ -0,0 +1,52 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* Includes ------------------------------------------------------------------*/
+
+#include "main.h"
+#include <stm32h5xx.h>
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private user code ---------------------------------------------------------*/
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+  // Configure the System Clock
+  SystemClock_Config();
+
+  // Enable the Instruction Caching
+  MX_ICACHE_Init();
+
+  while (1)
+  {
+    __NOP();
+  };
+}
