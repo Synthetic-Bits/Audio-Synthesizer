17134bc72b4b02fdb4c801076151d61c0948985f
AdrianSucahyo <adrianpsucahyo@gmail.com>
2025-04-24 11:31:42 -0600

    Update midi.h

--- Audio_Synthesizer_H533/Core/Inc/midi.h
+++ Audio_Synthesizer_H533/Core/Inc/midi.h
@@ -1,3 +1,6 @@
+#ifndef _MIDI_H_
+#define _MIDI_H_
+
 #include <stdint.h>
 #include "sample_timer.h"
 #include "channel_common.h"
@@ -5,3 +8,5 @@
 void setup_midi(void);
 void set_midi(char data[]);
 void midi_channle_voice_init(void);
+
+#endif
